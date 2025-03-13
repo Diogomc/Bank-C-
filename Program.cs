f9947b0d9563a48d2a1faacb9bd939ab7f0ae379
Diogo <diogo.marcondes2004@gmail.com>
2025-03-12 21:30:19 -0300

    Code Added

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.WriteLine("-------------------------------");
+
+
+
+int scan = 0;
+
+while (scan != 4)
+{
+
+    Console.WriteLine("1 - Consultar Saldo");
+    Console.WriteLine("2 - Sacar");
+    Console.WriteLine("3 - Depositar");
+    Console.WriteLine("4 - Sair");
+
+    scan = Convert.ToInt16(Console.ReadLine());
+    {
+
+        switch (scan)
+        {
+            case 1:
+                Console.WriteLine("Apertou 1");
+                break;
+            case 2:
+                Console.WriteLine("Apertou 2");
+                break;
+            default:
+                Console.WriteLine("Número inválido");
+                break;
+        }
+    }
+}
+
